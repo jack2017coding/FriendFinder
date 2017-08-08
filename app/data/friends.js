c3a004c1167cf028fe001d3cc351ca20ccee138f
harbasheng <jack.bashirian@harbash.com>
2017-08-08 00:36:38 -0400

    rev0

---
+++ app/data/friends.js
@@ -0,0 +1,68 @@
+var friends = [
+  {
+  "name":"Isaac",
+  "photo":"http://static-13.sinclairstoryline.com/resources/media/0bf63340-51c4-4d14-a1b9-ce484ee2905b-large16x9_Wolf_Logo.jpg",
+  "scores":[
+     3,
+     2,
+     4,
+     1,
+     3,
+     2,
+     1,
+     2,
+     3,
+     4
+      ]
+},
+  {
+  "name":"Peter",
+  "photo":"http://static-13.sinclairstoryline.com/resources/media/0bf63340-51c4-4d14-a1b9-ce484ee2905b-large16x9_Wolf_Logo.jpg",
+  "scores":[
+     2,
+     1,
+     4,
+     3,
+     5,
+     1,
+     5,
+     5,
+     2,
+     3
+      ]
+},
+{
+  "name":"Alex",
+  "photo":"http://static-13.sinclairstoryline.com/resources/media/0bf63340-51c4-4d14-a1b9-ce484ee2905b-large16x9_Wolf_Logo.jpg",
+  "scores":[
+     1,
+     3,
+     4,
+     2,
+     5,
+     3,
+     2,
+     4,
+     5,
+     2
+      ]
+},
+{
+  "name":"Ernest",
+  "photo":"http://static-13.sinclairstoryline.com/resources/media/0bf63340-51c4-4d14-a1b9-ce484ee2905b-large16x9_Wolf_Logo.jpg",
+  "scores":[
+     2,
+     1,
+     5,
+     3,
+     1,
+     3,
+     4,
+     2,
+     1,
+     2
+      ]
+}
+];
+
+module.exports = friends;
